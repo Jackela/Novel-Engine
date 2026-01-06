6660218e1d1521bece45b19a738dc4ef04081158
Jackela <k740724287@gmail.com>
2026-01-06 08:47:49 +0800

    Fix api_server router logging for APIRouter

--- api_server.py
+++ api_server.py
@@ -30,7 +30,10 @@ try:
     from apps.api.http import world_router as _world_router  # noqa: F401
 
     WORLD_ROUTER_AVAILABLE = True
-    logger.debug("World context router loaded: %s", _world_router.__name__)
+    logger.debug(
+        "World context router loaded: %s",
+        getattr(_world_router, "__name__", _world_router.__class__.__name__),
+    )
 except ImportError as exc:
     WORLD_ROUTER_AVAILABLE = False
     logger.warning("World context router not available: %s", exc)
