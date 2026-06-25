d091268a114a2126f2a1f601b1365f1c622c4302
Jackela <k740724287@gmail.com>
2026-06-25 18:56:31 +0800

    chore(deps-dev): sync react hooks lint update

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -30,6 +30,7 @@ export default [
     rules: {
       ...tsPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react-hooks/set-state-in-effect': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': [
         'error',
